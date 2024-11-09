bf52ce151621894876308fd011b0f3d68acc7332
QuickNOte <dineshreddyudumula143@gmail.com>
2024-11-09 10:19:23 +0530

    Initial 1

---
+++ app/SplashActivity.java
@@ -0,0 +1,15 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000); // 2 seconds delay
+    }
+}
